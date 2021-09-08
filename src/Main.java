728e38bfe61d42b52a8f00d503fb6cb18230a10e
Achraf <aitsimhandachraf@gmail.com>
2021-09-08 15:42:29 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,13 @@ public class Main {
             Connection myConn = DriverManager.getConnection("jdbc:postgresql://localhost/ovchip?user=postgres&password=rJFQu34u");
             Statement myStmnt = myConn.createStatement();
             ResultSet myRs = myStmnt.executeQuery("SELECT * from reiziger");
-            System.out.println("Alle reizigers");
+            System.out.println("Alle reizigers:");
 
             while(myRs.next()){
 
-                System.out.println("");
-                System.out.println(myRs.getString(1));
-                System.out.println(myRs.getString(2));
-                System.out.println(myRs.getString(3));
-                System.out.println(myRs.getString(4));
-                System.out.println(myRs.getString(5));
+                System.out.println("#" + myRs.getString(1) + ": "
+                        + myRs.getString(2) + ". " + myRs.getString(4)
+                        + " " + "(" + myRs.getString(5) + ")");
             }
 
         }
